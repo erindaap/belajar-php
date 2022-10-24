2bbe8f4c1aca18f752860cbe86955b1c3be64487
Erindah Putri <erindaputri5@gmail.com>
2022-10-24 11:14:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Hello!<br>";
+
+$nama = "Erinda";
+$umur = 20;
+
+echo "Nama saya $nama, saya berusia $umur.";
+
+$namaAdik = "Dian";
+$umurAdik = 15;
+
+echo "Nama adik saya $namaAdik, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+
+
+
+
+?>
