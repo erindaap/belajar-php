2f50d6da7c72dee6e261727bd37e14e050661994
Erindah Putri <erindaputri5@gmail.com>
2022-11-09 10:11:08 +0700

    table database

---
+++ mahasiswa.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE fakultas;
+
+CREATE TABLE jurusan (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    kode CHAR(4) NOT NULL,
+    nama VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE mahasiswa (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_jurusan INTEGER NOT NULL,
+    nim CHAR(8) NOT NULL,
+    nama VARCHAR(50) NOT NULL,
+    jenis_kelamin enum('laki-laki', 'perempuan') NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_jurusan) REFERENCES jurusan(id)
+);
+
+<img src= "Screenshot mysql.png" alt="id">
+
+
+
+
